d2ba1f75eb6a10da9f9d50bf3010a8b3290f7109
Bizki1337 <nightingale1749@yandex.ru>
2021-07-13 18:13:43 +0300

    updating app with reselect, replaced cart selectors with memorized selectors

--- src/components/cart-dropdown/Cart-dropdown.jsx
+++ src/components/cart-dropdown/Cart-dropdown.jsx
@@ -1,12 +1,25 @@
+import { connect } from 'react-redux';
+
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+
 import CustomButton from '../custom-button/CustomButton';
+import CartItem from '../cart-item/Cart-item';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
-        <div className='cart-items'/>
+        <div className='cart-items'>
+            {
+                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
+            }
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
-export default CartDropdown;
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
+})
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/Cart-icon.jsx
+++ src/components/cart-icon/Cart-icon.jsx
@@ -1,20 +1,28 @@
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, ItemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{ItemCount}</span>
     </div>
 )
+
+const mapStateToProps = (state) => ({
+    ItemCount: selectCartItemsCount(state)
+})
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(
+    mapStateToProps, 
+    mapDispatchToProps
+    )(CartIcon);
