9a93f3290c162c46aba3774c35d259732c20d808
Bizki1337 <nightingale1749@yandex.ru>
2021-07-15 17:01:25 +0300

    updating redux flow for components

--- src/components/collections-overview/Collections-overview.jsx
+++ src/components/collections-overview/Collections-overview.jsx
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCollections } from '../../redux/shop/shop.selector';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
 
 import CollectionPreview from '../collection-preview/Collection-preview';
 
@@ -18,7 +18,7 @@ const CollectionsOverview = ({ collections }) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview);
